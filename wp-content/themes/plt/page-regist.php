126950eab2bfc06539c8f068c979a7de34bcf3ab
cashely.shi <290119516@qq.com>
2016-12-02 16:11:17 +0800

    2016-12-2 01 增加报名流程

--- wp-content/themes/plt/page-regist.php
+++ wp-content/themes/plt/page-regist.php
@@ -8,12 +8,22 @@
 <!--content -->
     <div class="content login">
         <div class="login">
-            <h3 class="title">网上报名</h3>
-            <span class="line"></span>
-            
-            <?php while (have_posts()) : the_post(); ?>
-	        		<?php the_content();?>
-	        <?php endwhile;?>
+           <div class="process">
+               <img src="<?php bloginfo('template_url'); ?>/images/processImg.jpg" alt=""/>
+                    <div>
+                        <span class="process-left">网上报名</span>
+                        <span>现场提交资料确认，缴费</span>
+                        <span class="process-right">报名成功</span>
+                    </div>
+                </div>
+
+           <div class="titlebar">
+                <h3 class="title">网上报名</h3>
+                <span class="line"></span>
+           </div>
+           <?php while (have_posts()) : the_post(); ?>
+	        	<?php the_content();?>
+	       <?php endwhile;?>
         </div>
 <?php
 	get_footer();
